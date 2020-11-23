b2b0e84590a49e4dad63d1d9dedb2b67d75b1136
Anonym <anonym1120@gmail.com>
2020-11-23 15:50:33 +0800

    solve Views

---
+++ RentBook/RentBook/Controllers/messageBoardController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentBook.Models;
+
+namespace RentBook.Controllers
+{
+    public class messageBoardController : Controller
+    {
+        // bm_id 書籍留言序號
+        // b_id 書籍編號
+        // m_id 會員編號
+        // bm_Message 留言內容
+        // bm_MessageTime 留言時間
+        // bm_score 會員對書籍的評分
+        // m_Name dbo.Member資料表的會員暱稱
+        public ActionResult DeleteMessageBoard(int? bm_id) 
+        {
+            if (bm_id != null) 
+            {
+                CmessageBoard dCust = new CmessageBoard() { bm_id = (int)bm_id };
+                (new CmessageFactory()).delete_BooksMessage(dCust);
+            }
+            return RedirectToAction("書籍資訊");
+        }
+        public ActionResult SaveMessage() 
+        {
+            CmessageBoard x = new CmessageBoard();
+            //待改
+            //x.bm_id
+            //x.b_id
+            //x.m_id
+            x.bm_Message = Request.Form["txtMessage"];
+            x.bm_score = int.Parse(Request.Form["radioScore"]);
+
+            (new CmessageFactory()).create__BooksMessage(x);
+            return RedirectToAction("書籍資訊");
+
+        }
+        // GET: messageBoard
+        public ActionResult CreateMessageBoard()
+        {
+            return View();
+        }
+    }
+}
---
+++ RentBook/RentBook/Models/CmessageFactory.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace RentBook.Models
+{
+    public class CmessageFactory
+    {
+        // bm_id 書籍留言序號
+        // b_id 書籍編號
+        // m_id 會員編號
+        // bm_Message 留言內容
+        // bm_MessageTime 留言時間
+        // bm_score 會員對書籍的評分
+        // m_Name dbo.Member資料表的會員暱稱
+        public List<CmessageBoard> getAll_BooksMessage() 
+        {
+            return getBySql_BooksMessage("select * from BooksMessage ", null);
+        }
+        public CmessageBoard getByBmId_BooksMessage(int bm_id) 
+        {
+            List<SqlParameter> paras = new List<SqlParameter>();
+            paras.Add(new SqlParameter("BM_ID",(object)bm_id));
+
+            List<CmessageBoard> list = getBySql_BooksMessage("select * from BooksMessage where bm_id=@BM_ID", paras);
+            if (list.Count == 0) 
+            {
+                return null;
+            }
+
+            return list[0];
+        }
+
+        public void delete_BooksMessage(CmessageBoard p) 
+        {
+            List<SqlParameter> list = new List<SqlParameter>();
+            list.Add(new SqlParameter("M_ID",(object)p.m_id));
+            executeSql_BooksMessage("delete from BooksMessage where m_id=@M_ID", list);
+        }
+
+        private List<CmessageBoard> getBySql_BooksMessage(string sql, List<SqlParameter> paras) 
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=DESKTOP-QC55GV4\SQLEXPRESS;Initial Catalog=RentBookdb;Integrated Security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = sql;
+            if (paras != null)
+            {
+                foreach (SqlParameter p in paras)
+                {
+                    cmd.Parameters.Add(p);
+                }
+            }
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<CmessageBoard> list = new List<CmessageBoard>();
+            while (reader.Read()) 
+            {
+                CmessageBoard x = new CmessageBoard();
+                x.bm_id = (int)reader["bm_id"];
+                x.b_id = (int)reader["b_id"];
+                x.m_id = (int)reader["m_id"];
+                x.bm_Message = reader["bm_Message"].ToString();
+                x.bm_MessageTime = reader["bm_MessageTime"].ToString();
+                x.bm_score = (int)reader["bm_score"];
+                list.Add(x);
+            }
+            con.Close();
+
+            return list;
+
+
+
+            //return MessageBoard;
+        }
+        private void executeSql_BooksMessage(string sql, List<SqlParameter> paras)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=DESKTOP-QC55GV4\SQLEXPRESS;Initial Catalog=RentBookdb;Integrated Security=True";
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = sql;
+
+            if (paras != null) 
+            {
+                foreach (SqlParameter p in paras) 
+                {
+                    cmd.Parameters.Add(p);
+                }
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public void create__BooksMessage(CmessageBoard p) 
+        {
+            string sql = "insert into BooksMessage(";
+            sql += "bm_Message";
+            sql += ")values(";
+            sql += "@BM_MESSAGE)";
+
+            List<SqlParameter> paras = new List<SqlParameter>();
+            paras.Add(new SqlParameter("BM_MESSAGE",(object)p.bm_Message));
+            executeSql_BooksMessage(sql, paras);
+        }
+    }
+}
